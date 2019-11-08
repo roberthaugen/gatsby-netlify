d0e97d1defa5873b507a3dd734df727d0eef481f
Robert Haugen <robert@haugen.me>
2019-11-08 14:57:46 +0100

    optimize colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import footerStyles from "./Footer.module.css"
 const Footer = () => (
     <footer className={footerStyles.footer}>
         <div className="container">
-            <span className="text-muted">Copyleft: Robby</span>
+            <span className="text">Copyleft: Robby</span>
         </div>
     </footer>
 )
